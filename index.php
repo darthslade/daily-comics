6f1ef51269e9b6e859c3a2bf6905bef3abe25e87
Michael Galloway <michaelgalloway_35@hotmail.com>
2017-05-05 17:25:06 -0500

    Enable PHP to execute, read, & format Node output

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html');
+exec('mode=php /usr/local/bin/node index.js', $output);
+
+$doc = new DOMDocument();
+$doc->loadHTML(implode("\n", $output));
+
+// get current folder
+$body = $doc->getElementsByTagName('body')->item(0);
+$folder = $body->getAttribute('data-folder');
+
+// update all images paths to include current folder
+$images = $doc->getElementsByTagName('img');
+foreach($images as $image) {
+    $src = $image->getAttribute('src');
+    $image->setAttribute('src', "$folder/$src");
+}
+// display results
+echo $doc->saveHTML();
